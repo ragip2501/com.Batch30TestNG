45181a59dc8720600b2882201393a1af827a8b0e
ragip2501 <ragip2501@gmail.com>
2021-10-16 23:29:19 +0300

    gün sonu

---
+++ src/test/java/tests/day11/C04_FileDownload.java
@@ -0,0 +1,17 @@
+package tests.day11;
+
+import org.testng.annotations.Test;
+
+public class C04_FileDownload {
+
+    @Test
+    public void test(){
+        //1. Tests packagenin altina bir class oluşturalim : C04_FileDownload
+        //2. Iki tane metod oluşturun : isExist() ve downloadTest()
+        //3. downloadTest () metodunun icinde aşağıdaki testi yapalim:
+        //      - https://the-internet.herokuapp.com/download adresine gidelim.
+        //      - logo.png dosyasını indirelim
+        //4. Ardından isExist()  methodunda dosyanın başarıyla indirilip indirilmediğini test edelim
+
+    }
+}
